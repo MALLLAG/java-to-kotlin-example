0a8dd666192141de8940737bf29fb6a43e0df8f4
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-01-19 22:30:54 +0900

    CHAPTER 4 예제 코드 추가

---
+++ src/main/java/com/example/demo/nullability/Leg.java
@@ -0,0 +1,29 @@
+package com.example.demo.nullability;
+
+import java.time.Duration;
+
+public class Leg {
+    private final String name;
+    private final Duration plannedDuration;
+
+    private Leg(String name, Duration plannedDuration) {
+        this.name = name;
+        this.plannedDuration = plannedDuration;
+    }
+
+    public static Leg leg(String name, Duration duration) {
+        return new Leg(name, duration);
+    }
+
+    public Duration getPlannedDuration() {
+        return plannedDuration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return name;
+    }
+}
