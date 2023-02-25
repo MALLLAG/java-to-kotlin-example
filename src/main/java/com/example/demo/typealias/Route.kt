0dddd1c805de968b972f65d4e3cd99036515b077
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-02-25 15:11:58 +0900

    CHAPTER 15 예제 코드 추가

---
+++ src/main/java/com/example/demo/typealias/Route.kt
@@ -0,0 +1,20 @@
+package com.example.demo.`typealias`
+
+typealias Route = List<Journey>
+
+val Route.departsFrom: Location
+    get() = first().departsFrom
+
+val Route.arrivesAt: Location
+    get() = last().arrivesAt
+
+val Route.duration: Duration
+    get() = Duration.between(
+        first().departureTime,
+        last().arrivalTime
+    )
+
+fun <T> Iterable<T>.withItemAt(index: Int, replacedBy: T): List<T> =
+    this.toMutableList().apply {
+        this[index] = replacedBy
+    }
